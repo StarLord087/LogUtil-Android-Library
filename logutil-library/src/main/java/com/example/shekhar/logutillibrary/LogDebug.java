39e62420d8fa60809851e223e91d27a54cd985de
Shekhar <shekahr.singh087@gmail.com>
2017-11-04 00:47:19 +0530

    Function added

--- logutil-library/src/main/java/com/example/shekhar/logutillibrary/LogDebug.java
+++ logutil-library/src/main/java/com/example/shekhar/logutillibrary/LogDebug.java
@@ -1,10 +1,17 @@
 package com.example.shekhar.logutillibrary;
 
+import android.util.Log;
+
 /**
  * Created by shekh on 03-11-2017.
  */
 
 public class LogDebug {
 
+    public static final String TAG = "STARLORD";
+
+    public static void d(String message) {
+        Log.d(TAG, message);
+    }
 
 }
