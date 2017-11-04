301ebfbfe594a65128d3cd088c51f45afe25190d
Shekhar <shekahr.singh087@gmail.com>
2017-11-04 09:40:35 +0530

    Add error debugging module

--- app/src/main/java/com/example/shekhar/logutilexample/MainActivity.java
+++ app/src/main/java/com/example/shekhar/logutilexample/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.shekhar.logutilexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.shekhar.logutillibrary.LogDebug;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    LogDebug.d("Shekhar Singh" );
     }
 }
---
+++ error-util/src/main/java/com/example/shekhar/errorutil/ErrorDebug.java
@@ -0,0 +1,20 @@
+package com.example.shekhar.errorutil;
+
+import android.util.Log;
+
+/**
+ * Created by shekh on 04-11-2017.
+ */
+
+public class ErrorDebug {
+
+    public static final String TAG = "STARLORD";
+
+    public static void e(String message) {
+        Log.e(TAG, message);
+
+
+    }
+
+
+}
